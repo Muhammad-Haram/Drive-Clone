812b25557b36d0b41a80424cde2371b2390a2a9f
Muhammad-Haram <haramh643@gmail.com>
2025-01-14 17:10:07 +0500

    create register form

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
+const userRouter = require("./routes/user.routes.js");
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use("/user", userRouter);
 
 app.listen(3000, () => {
   console.log("server started on port 3000");
