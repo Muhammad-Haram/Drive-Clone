090d0b031a42456269c46943995387e1d05ba0cc
Muhammad-Haram <haramh643@gmail.com>
2025-01-14 18:40:47 +0500

    commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const userRouter = require("./routes/user.routes.js");
 
 app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", userRouter);
 
